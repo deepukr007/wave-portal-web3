bebaa8682140547f9b12b71d0374f23430c74d73
unknown <deepukreddy123@gmail.com>
2021-11-25 03:23:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      👋 Hey there!
       </header>
+      <div className="tagLine">
+         I am Deepu and I love to build stuff | Muley | 📚Masters Student @University of Freiburg 
+      </div>
+      <div className="waveButton">
+         <button>Wave at me </button>
+      </div>
     </div>
   );
 }
